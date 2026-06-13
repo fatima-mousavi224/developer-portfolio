8e932989bd8a60f9ce05cc47eda8e56a9f4e6fb9
fatima-mousavi224 <fatimamousavi224@gmail.com>
2026-06-13 15:02:22 +0430

    Add Project

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -10,7 +10,16 @@ export const projectsData = [
     demo: "https://netflix-ivory-nine.vercel.app/",
   },
   {
-    id: 2,
+  id: 2, 
+  name: "PennyPilot",
+  description: "A smart personal finance dashboard featuring dynamic data charts, secure user account isolation, and an automated AI spending coach.",
+  tools: ["Next.js", "Supabase", "Tailwind CSS", "Recharts"],
+  role: "Full-Stack Developer",
+  code: "https://github.com/fatima-mousavi224/PennyPilot_dashboard", 
+  demo: "https://penny-pilot-dashboard.vercel.app/"
+},
+  {
+    id: 3,
     name: "Easy Bank",
     description: "A landing page for a modern banking app with clean UI and responsive layout.",
     tools: ["HTML", "CSS", "JavaScript"],
@@ -20,7 +29,7 @@ export const projectsData = [
   },
   
     {
-    id: 3,
+    id: 4,
     name: "Country Quiz App",
     description: "A quiz application using API to test knowledge of countries and geography.",
     tools: ["JavaScript", "API", "HTML", "CSS"],
@@ -29,7 +38,7 @@ export const projectsData = [
     demo: "https://country-quiz-mousavi.netlify.app/"
   },
   {
-    id: 4,
+    id: 5,
     name: "Browser Extensions Manager",
     description: "A UI dashboard to manage browser extensions with toggle functionality.",
     tools: ["JavaScript", "HTML", "CSS"],
